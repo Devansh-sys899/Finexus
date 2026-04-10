e133e54770013ca68714121bfcc9fb4c0e805e3a
Devansh-sys899 <dishanahar791@gmail.com>
2026-04-10 20:55:22 +0530

    fixed the auth issue

--- Finexus/Frontend/src/api/axios.js
+++ Finexus/Frontend/src/api/axios.js
@@ -1,30 +1,19 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://finexus-5h8c.onrender.com",
-  withCredentials: true 
+  baseURL: "https://finexus-5h8c.onrender.com", 
+  withCredentials: true, 
 });
 
-// Attach Authorization token to every request
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// Intercept 401 Unauthorized globally and redirect to login if we lose auth
 instance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      localStorage.removeItem("token");
-      // Basic redirect if we're not on auth pages
-      if (window.location.pathname !== "/login" && window.location.pathname !== "/signup" && window.location.pathname !== "/") {
+      if (
+        window.location.pathname !== "/login" &&
+        window.location.pathname !== "/signup" &&
+        window.location.pathname !== "/"
+      ) {
         window.location.href = "/login";
       }
     }
